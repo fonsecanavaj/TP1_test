28928ec4098812199e85307c88b2446fc9e3057f
fonsecanavaj <fonsecanavaj@educ.college-montreal.qc.ca>
2023-08-31 16:18:18 -0400

    test2 - class 1.2

--- main.py
+++ main.py
@@ -2,11 +2,8 @@
 number1 = int(input('number 1'))
 number2 = int(input('number 2'))
 def addition1(nmb1, nmb2):
-    somme = nmb1 + nmb2
-    return somme
 
-addition2 = addition1(number1, number2) +3
-print(addition2)
+
 
 print(addition1(1,5))
 #message = input("Enter an all lowercase message please \n")
