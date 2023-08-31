18b586f4deb1e2e231659370a1cd1ad387cbee6a
fonsecanavaj <fonsecanavaj@educ.college-montreal.qc.ca>
2023-08-31 16:17:51 -0400

    test2 - class 1.2

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-message = input("Enter an all lowercase message please")
-number = message
+
+number1 = int(input('number 1'))
+number2 = int(input('number 2'))
+def addition1(nmb1, nmb2):
+    somme = nmb1 + nmb2
+    return somme
+
+addition2 = addition1(number1, number2) +3
+print(addition2)
+
+print(addition1(1,5))
+#message = input("Enter an all lowercase message please \n")
+#number = len(message.split(" "))
+#print(f"The number is {number} words")
