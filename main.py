a4c8d330aae206340cf4ff34d4392f2a29f2948a
fonsecanavaj <143746128+fonsecanavaj@users.noreply.github.com>
2023-09-09 00:39:27 -0400

    Update main.py
    new code - final version

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-
-number1 = int(input('number 1'))
-number2 = int(input('number 2'))
-def addition1(nmb1, nmb2):
-
+# Julieta María Fonseca Nava 
+# 03/09/23
+# TP1 - Word_Count
 
 
-print(addition1(1,5))
-#message = input("Enter an all lowercase message please \n")
-#number = len(message.split(" "))
-#print(f"The number is {number} words")
+# Obtenir de l'utilisateur la phrase à compter 
+message = input("Écrivez une phrase contenant seulement des mots en minuscules ainsi que des espaces.\n")
+
+
+# Séparer la phrase en une liste (mots) et compter les éléments
+# Input: Le message (string)
+# Output: Nbr de mots (int)
+def count_word(phrase):
+    word_list = phrase.split(" ")
+    word_lenght = len(word_list)
+    return word_lenght
+
+
+# Afficher le nombre exact de mots 
+print(f"Le nombre de mots dans votre phrase est de {count_word(message)} mots.")
